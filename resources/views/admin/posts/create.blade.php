28fdf4d89de34381b6632415e610d017544a8850
AleColumn99 <alecolumn.99@gmail.com>
2021-07-04 14:33:18 +0200

    controllo

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,6 +3,21 @@
 @section('content')
 <div class="container">
     <h1>Nuovo post</h1>
+
+    @if ($errors->any())
+
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>
+                        {{ $error }}
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+
+    @endif
+
     <div>
         <form action="{{ route('admin.posts.store') }}" method="POST">
             @csrf
@@ -10,12 +25,22 @@
 
             <div class="mb-3">
                 <label class="label-control" for="title">Titolo</label>
-                <input type="text" id="title" name="title"  class="form-control">
+                <input type="text" id="title" name="title" value="{{ old('title') }}" class="form-control @error('title') is-invalid @enderror">
+                @error('title')
+                    <div class="text-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label class="label-control" for="content">Content</label>
-                <textarea type="text" id="content" name="content" class="form-control" rows="5" ></textarea>
+                <textarea type="text" id="content" name="content" value="{{ old('content') }}" class="form-control @error('content') is-invalid @enderror" rows="5" ></textarea>
+                @error('content')
+                    <div class="text-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div>
                 <button class="btn btn-primary" type="submit">Invio</button>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PageController@index' );
+//Route::get('/', 'PageController@index' );
 
 Auth::routes();
 
@@ -28,3 +28,5 @@ Route::prefix('admin')
         Route::get('/','HomeController@index')->name('home');
         Route::resource('/posts', 'PostController');
     });
+
+Route::get('{any?}', 'PageController@index')->where('any','.*');
